bf2ee03b99f35c3d66caf53dcb14df31902c4187
mykola.tsyb <mykola.tsyb@oracle.com>
2024-04-22 18:26:21 +0200

    add lesson 16

---
+++ lesson_16_security_jwt/src/test/java/org/example/lesson_n_security/LessonNSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+//package org.example.lesson_n_security;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class LessonNSecurityApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
